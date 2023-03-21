4e5776b5469d0643e9ee3fc0e592d7f88c541bc6
kumomiki <rita21625@gmail.com>
2023-03-20 23:05:36 -0500

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import 'src/style/reset.scss';
 import 'src/style/base.scss';
 import { Sidebar } from 'src/components/Sidebar/Sidebar';
 import { PopularListSection } from 'src/components/PopularListSection/PopularListSection';
+import { Header } from 'src/components/Header/Header';
 
 // import { compileString } from "sass";
 
@@ -10,6 +11,7 @@ function App() {
 		<div className='App'>
 			<h1>Twitter</h1>
 			<Sidebar />
+			<Header header='首頁' />
 			<PopularListSection />
 		</div>
 	);
