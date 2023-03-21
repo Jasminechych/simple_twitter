3a347675ee8f814ac60990e5adfc55bebac36550
kumomiki <rita21625@gmail.com>
2023-03-20 19:07:41 -0500

    delete popularListSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import 'src/style/reset.scss';
 import 'src/style/base.scss';
 // import { compileString } from "sass";
-import { Sidebar } from 'src/components/Sidebar/Sidebar';
-import { PopularListSection } from 'src/components/PopularListSection/PopularListSection';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Twitter</h1>
-			<Sidebar />
-			<PopularListSection />
 		</div>
 	);
 }
