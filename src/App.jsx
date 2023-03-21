1fd4cb4a0e01f7bd6f8f3a4febb939363f787f3d
kumomiki <rita21625@gmail.com>
2023-03-20 23:22:40 -0500

    feat: add mainSection component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import 'src/style/reset.scss';
 import 'src/style/base.scss';
 import { Sidebar } from 'src/components/Sidebar/Sidebar';
 import { PopularListSection } from 'src/components/PopularListSection/PopularListSection';
-import { Header } from 'src/components/Header/Header';
+import { MainSection } from 'src/components/MainSection/MainSection';
 
 // import { compileString } from "sass";
 
@@ -11,7 +11,7 @@ function App() {
 		<div className='App'>
 			<h1>Twitter</h1>
 			<Sidebar />
-			<Header header='首頁' />
+			<MainSection />
 			<PopularListSection />
 		</div>
 	);
