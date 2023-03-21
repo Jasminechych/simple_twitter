2872246f93c57e7588998093331f4862b12081ea
kumomiki <rita21625@gmail.com>
2023-03-21 16:35:56 -0500

    fix: change title name

--- src/components/ReplyListTab/ReplyListTab.jsx
+++ src/components/ReplyListTab/ReplyListTab.jsx
@@ -3,7 +3,7 @@ import style from 'src/components/ReplyListTab/ReplyListTab.module.scss';
 export const ReplyListTab = () => {
 	return (
 		<div className={style.replyListTabContainer}>
-			<div className={style.replyListTabTitle}>推文</div>
+			<div className={style.replyListTabTitle}>回覆</div>
 		</div>
 	);
 };
