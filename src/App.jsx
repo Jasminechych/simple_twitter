502ba892e624b59444bb4d85d0912578696449d5
jasminechych <jasminechych@gmail.com>
2023-03-22 00:03:43 +0800

    feat: add AuthInput component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Sidebar } from 'src/components/Sidebar/Sidebar';
 import { PopularListSection } from 'src/components/PopularListSection/PopularListSection';
 import { MainSection } from 'src/components/MainSection/MainSection';
 import { ButtonXL, ButtonL, ButtonM, ButtonS, ButtonSW, ButtonXS } from 'src/components/buttons';
+import { AuthInput } from 'src/components/AuthInput/AuthInput';
 // import { compileString } from "sass";
 
 function App() {
@@ -19,6 +20,8 @@ function App() {
 			<ButtonS text='ButtonS' />
 			<ButtonSW text='ButtonSW' />
 			<ButtonXS text='ButtonXS' />
+			<AuthInput label='名稱' title='name' type='text' placeholder='請輸入帳號' maxLength='2' />
+			<AuthInput label='密碼' title='password' type='password' placeholder='請輸入密碼' />
 		</div>
 	);
 }
