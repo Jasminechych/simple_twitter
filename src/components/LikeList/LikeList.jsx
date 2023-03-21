422e82683a4e31a57e9fd7924396035f71386fb5
kumomiki <rita21625@gmail.com>
2023-03-21 16:34:32 -0500

    fix: remove a div and its style

--- src/components/LikeList/LikeList.jsx
+++ src/components/LikeList/LikeList.jsx
@@ -4,7 +4,6 @@ export const LikeList = () => {
 	return (
 		<div className={style.likeListContainer}>
 			<div className={style.likeListTitle}>喜歡的內容</div>
-			<div className={style.likeListTweets}>LikeTweets</div>
 		</div>
 	);
 };
