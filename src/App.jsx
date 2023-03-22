27fb4d2830378b5a5da9e1a422ef6a03acbe4b26
jasminechych <jasminechych@gmail.com>
2023-03-22 12:19:58 +0800

    feat: add SignInPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'src/style/reset.scss';
 import 'src/style/base.scss';
-import { MainPage, RegisterPage } from 'src/pages';
+import { MainPage, RegisterPage, SignInPage } from 'src/pages';
 
 // import { compileString } from "sass";
 
@@ -9,6 +9,7 @@ function App() {
 		<div className='App'>
 			<MainPage />
 			<RegisterPage />
+			<SignInPage />
 		</div>
 	);
 }
