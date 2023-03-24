c7ab404c7674120213e60e182b43bcd750393c0d
jasminechych <jasminechych@gmail.com>
2023-03-24 22:48:06 +0800

    style: delete comments of AdminMainPage

--- src/pages/AdminMainPage/AdminMainPage.jsx
+++ src/pages/AdminMainPage/AdminMainPage.jsx
@@ -1,36 +1,12 @@
-// import { useState } from 'react';
 import { AdminSidebar } from 'src/components/AdminSidebar/AdminSidebar';
-// import { AdminMainSection } from 'src/components/AdminMainSection/AdminMainSection';
-// import { TweetList } from 'src/components/TweetList/TweetList';
-// import { UserList } from 'src/components/UserList/UserList';
 import style from 'src/pages/AdminMainPage/AdminMainPage.module.scss';
 import { Outlet } from 'react-router-dom';
 
 export const AdminMainPage = () => {
-	// const navigate = useNavigate();
-
-	// const [page, setPage] = useState('tweetList');
-	// console.log('page: ', page);
-
-	// const handlePageChange = (page) => {
-	// 	setPage(page);
-	// };
-
 	return (
 		<div className={style.page}>
 			<AdminSidebar />
 			<Outlet />
-			{/* <AdminSidebar handlePageChange={handlePageChange} />
-			{page === 'tweetList' && (
-				<AdminMainSection pageTitle='推文清單'>
-					<TweetList />
-				</AdminMainSection>
-			)}
-			{page === 'userList' && (
-				<AdminMainSection pageTitle='使用者列表'>
-					<UserList />
-				</AdminMainSection>
-			)} */}
 		</div>
 	);
 };
