b3f857ec47d876e19468f7bc799f62bb4bdb35f2
kumomiki <rita21625@gmail.com>
2023-03-26 16:41:40 -0500

    fix: remove popularListSection at setting page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,16 @@
+import { useLocation } from 'react-router-dom';
 import { PopularListSection } from 'src/components/PopularListSection/PopularListSection';
 import { Sidebar } from 'src/components/Sidebar/Sidebar';
 import style from 'src/pages/MainPage/MainPage.module.scss';
 
 export const MainPage = ({ children }) => {
+	const location = useLocation();
+	const currentPath = location.pathname;
 	return (
 		<div className={style.mainPageContainer}>
 			<Sidebar />
 			{children}
-			<PopularListSection />
+			{currentPath !== '/setting' && <PopularListSection />}
 		</div>
 	);
 };
