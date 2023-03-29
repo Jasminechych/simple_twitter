1011e47e3f6fb3f811b88c2aa4fab22a86ef4f6b
kumomiki <rita21625@gmail.com>
2023-03-29 17:00:31 -0500

    fix: store data flow

--- src/components/Modal/EditModal/EditModal.jsx
+++ src/components/Modal/EditModal/EditModal.jsx
@@ -2,27 +2,24 @@ import style from 'src/components/Modal/EditModal/EditModal.module.scss';
 import { AuthInput } from 'src/components/AuthInput/AuthInput';
 import { AddPhoto, Close, Cover } from 'src/assets/icons';
 import { ButtonS } from 'src/components/buttons';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { getUserData } from 'src/apis/user';
+import { getUserData, putUserData } from 'src/apis/user';
+import Swal from 'sweetalert2';
 
 export const EditModal = () => {
-	const current = JSON.parse(localStorage.getItem('currentUser'));
+	const navigate = useNavigate();
+	// const current = JSON.parse(localStorage.getItem('currentUser'));
 	// console.log('setting page current: ', current);
 
 	// 	使用者取得自己的資料
 	const [initialValues, setInitialValues] = useState({
-		id: current.currentUserId,
-		name: current.currentUserName,
-		avatar: current.currentUserAvatar,
-		cover: current.currentUserCover,
-		intro: current.currentUserIntroduction,
+		name: '',
+		avatar: '',
+		cover: '',
+		introduction: '',
 	});
 	console.log('initialValues:', initialValues);
-
-	// 輸入時同步取得,可修改名稱和自我介紹
-	const [name, setName] = useState(initialValues.name);
-	const [intro, setIntro] = useState(initialValues.intro);
 
 	// 取得
 	useEffect(() => {
@@ -31,13 +28,15 @@ export const EditModal = () => {
 			console.log('currentUserId: ', currentUserId.currentUserId);
 			try {
 				const data = await getUserData(currentUserId.currentUserId);
-				setInitialValues({
-					id: data.id,
-					name: data.name,
-					avatar: data.avatar,
-					cover: data.cover,
-					intro: data.intro,
-				});
+				if (data) {
+					setInitialValues({
+						id: data.id,
+						name: data.name,
+						avatar: data.avatar,
+						cover: data.cover,
+						introduction: data.introduction,
+					});
+				}
 			} catch (error) {
 				console.error(error);
 			}
@@ -45,14 +44,58 @@ export const EditModal = () => {
 		getUsersInfo();
 	}, []);
 
+	// 輸入時同步取得,可修改名稱和自我介紹
+	// const [name, setName] = useState(initialValues.name);
+	// const [introduction, setIntro] = useState(initialValues.introduction);
+
 	//
-	const handleSave = () => {
+	const handleSave = async () => {
 		// 輸入框若有任一為空，防止表單送出，且跳出提示視窗
-		if (!name.trim().length || !intro.trim().length) {
+		if (!initialValues.name.trim().length || !initialValues.introduction.trim().length) {
 			return;
 		}
-		// 提交表單資料
-		console.log('submit form data', { name, intro });
+
+		try {
+			const currentUserId = JSON.parse(localStorage.getItem('currentUser'));
+			console.log('currentUserId: ', currentUserId.currentUserId);
+
+			// 帶入id，把更新的資料傳回後端
+			const id = currentUserId.currentUserId;
+			const response = await putUserData(id, {
+				name: initialValues.name,
+				introduction: initialValues.introduction,
+			});
+			console.log('Data updated successfully', response);
+			// console.log('response.data:', response.data);
+			const success = response.statusText;
+			if (success === 'OK') {
+				Swal.fire({
+					title: '儲存成功',
+					icon: 'success',
+					showConfirmButton: false,
+					position: 'top',
+					timer: 1000,
+				});
+
+				navigate('/user/self');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+
+		// const handleChangeName = (e) => {
+		// 	setInitialValues({
+		// 		...initialValues,
+		// 		name: e.target.value,
+		// 	});
+		// };
+
+		// const handleChangeIntro = (e) => {
+		// 	setInitialValues({
+		// 		...initialValues,
+		// 		introduction: e.target.value,
+		// 	});
+		// };
 	};
 
 	return (
@@ -85,9 +128,14 @@ export const EditModal = () => {
 						type='text'
 						placeholder='請輸入使用者名稱'
 						maxLength='50'
-						value={name}
-						errorMessage={!name.trim().length && '內容不可空白'}
-						onChange={(nameInputValue) => setName(nameInputValue)}
+						value={initialValues.name}
+						errorMessage={!initialValues.name.trim().length && '內容不可空白'}
+						onChange={(nameInputValue) =>
+							setInitialValues({
+								...initialValues,
+								name: nameInputValue,
+							})
+						}
 					/>
 
 					<AuthInput
@@ -96,10 +144,15 @@ export const EditModal = () => {
 						type='text'
 						placeholder='自我介紹'
 						maxLength='160'
-						value={intro}
-						errorMessage={!intro.trim().length && '內容不可空白'}
-						onChange={(introInputValue) => setIntro(introInputValue)}
-						style={{ height: '147px !important' }}
+						value={initialValues.introduction}
+						errorMessage={!initialValues.introduction.trim().length && '內容不可空白'}
+						onChange={(introInputValue) =>
+							setInitialValues({
+								...initialValues,
+								introduction: introInputValue,
+							})
+						}
+						style={{ height: '147px' }}
 					/>
 				</div>
 			</div>
