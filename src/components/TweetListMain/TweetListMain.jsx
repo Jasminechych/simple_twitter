2dc0903bc87dd010a1e206384add7ca9385110c6
jasminechych <jasminechych@gmail.com>
2023-04-01 11:55:11 +0800

    refactor: TweetListMain

--- src/components/TweetListMain/TweetListMain.jsx
+++ src/components/TweetListMain/TweetListMain.jsx
@@ -1,7 +1,7 @@
 import { TweetItem } from 'src/components/TweetItem/TweetItem';
-import style from 'src/components/TweetList/TweetList.module.scss';
+import style from 'src/components/TweetListMain/TweetListMain.module.scss';
 import { useState, useEffect } from 'react';
-import { getTweets, getUserLikes, getUserTweets } from 'src/apis/user';
+import { getTweets, getUserLikes } from 'src/apis/user';
 import { postLikeTweet, postUnLikeTweet } from 'src/apis/user';
 import { useCallback } from 'react';
 import { useMemo } from 'react';
@@ -9,70 +9,46 @@ import { useNavigate } from 'react-router-dom';
 import { convertDateToHours } from 'src/utils/convertDateToHours';
 import { useUserData } from 'src/context/UserContext';
 
-export const TweetList = ({ tab }) => {
+export const TweetListMain = () => {
+	// 目前登入的使用者 ID
 	const currentUserId = JSON.parse(localStorage.getItem('currentUser')).currentUserId;
 
 	// const [tweetListData, setTweetListData] = useState([]);
-	const [isDataLoaded, setIsDataLoaded] = useState(false);
-	const [userLikeData, setUserLikeData] = useState([]);
-	const [isUserLikeDataLoaded, setIsUserLikeDataLoaded] = useState(false);
+	const [isTweetListMainDataLoaded, setIsTweetListMainDataLoaded] = useState(false);
+	// const [userLikeData, setUserLikeData] = useState([]);
+	// const [isUserLikeDataLoaded, setIsUserLikeDataLoaded] = useState(false);
 	const [isLikingOrUnLiking, setIsLikingOrUnLiking] = useState({ id: '', likeOrUnlike: '' });
 	const [isHeartClick, setIsHeartClick] = useState(false);
 	const navigate = useNavigate();
-	const [isUserTweetsDataLoaded, setIsUserTweetsDataLoaded] = useState(false);
+	// const [isUserTweetsDataLoaded, setIsUserTweetsDataLoaded] = useState(false);
 
-	const { tweetsData, setTweetsData, userTweetsData, setUserTweetsData } = useUserData();
+	const { tweetsData, setTweetsData, userLikeData, setUserLikeData } = useUserData();
 
 	// 查看全站所有推文
 	useEffect(() => {
-		const getTweetsAsync = async () => {
+		const fetchTweetListMainAsync = async () => {
 			try {
-				const data = await getTweets();
-				// 待刪改
-				setTweetsData(data);
-				setIsDataLoaded(true);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-		getTweetsAsync();
-	}, [isHeartClick]);
-
-	// 查看使用者所有推文
-	useEffect(() => {
-		const getUserTweetsAsync = async () => {
-			try {
-				const data = await getUserTweets();
-				// 待刪改
-				setUserTweetsData(data);
-				setIsUserTweetsDataLoaded(true);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-		getUserTweetsAsync();
-	}, [isHeartClick]);
-
-	// 查看使用者喜歡過的推文
-	useEffect(() => {
-		const getUserLikesAsync = async () => {
-			try {
-				// 取得token
-				const token = localStorage.getItem('token');
-
 				// 先驗證token，若無則直接回到signin
+				const token = localStorage.getItem('token');
 				if (!token) {
 					navigate('signin');
 					return;
 				}
-				const data = await getUserLikes(currentUserId);
-				setUserLikeData(data);
-				setIsUserLikeDataLoaded(true);
+
+				// 取得全站所有推文
+				const getTweetsData = await getTweets();
+				setTweetsData(getTweetsData);
+
+				// 取得使用者喜歡的推文
+				const getUserLikesData = await getUserLikes(currentUserId);
+				setUserLikeData(getUserLikesData);
+
+				setIsTweetListMainDataLoaded(true);
 			} catch (error) {
 				console.error(error);
 			}
 		};
-		getUserLikesAsync();
+		fetchTweetListMainAsync();
 	}, [isHeartClick]);
 
 	// 對貼文按愛心或取消愛心
@@ -80,10 +56,10 @@ export const TweetList = ({ tab }) => {
 	const handleHeartClick = useCallback((id, likeOrUnlike) => {
 		console.log('click id: ', id, likeOrUnlike);
 		setIsLikingOrUnLiking({ id: id, likeOrUnlike: likeOrUnlike });
-		setIsDataLoaded(false);
-		setIsUserLikeDataLoaded(false);
+		setIsTweetListMainDataLoaded(false);
 	}, []);
 
+	// 對推文按讚或取消按讚
 	useEffect(() => {
 		const postLikeOrUnlikeTweetAsync = async () => {
 			try {
@@ -108,39 +84,19 @@ export const TweetList = ({ tab }) => {
 	// 比對使用者喜歡的貼文資料
 	// 讓使用者喜歡貼文清單帶上 isLikeByUser: true
 	const matchData = useMemo(() => {
-		if (
-			(!isDataLoaded && !isUserLikeDataLoaded) ||
-			(!isUserTweetsDataLoaded && !isUserLikeDataLoaded)
-		)
-			return [];
+		if (!isTweetListMainDataLoaded) return [];
 
-		//首頁全部推文清單
-		if (tab === 'totalTweetList') {
-			return tweetsData.map((item) => {
-				const isLiked = userLikeData.some((likeItem) => likeItem.TweetId === item.id);
-				return { ...item, isLikeByUser: isLiked };
-			});
-		}
+		return tweetsData.map((item) => {
+			const isLiked = userLikeData.some((likeItem) => likeItem.TweetId === item.id);
+			return { ...item, isLikeByUser: isLiked };
+		});
+	}, [isTweetListMainDataLoaded, tweetsData, userLikeData]);
 
-		// 個人資料頁面使用者全部推文
-		if (tab === 'tweetList' || tab === undefined) {
-			return userTweetsData.map((item) => {
-				const isLiked = userLikeData.some((likeItem) => likeItem.TweetId === item.id);
-				return { ...item, isLikeByUser: isLiked };
-			});
-		}
-
-		// 個人資料頁面使用者喜歡貼文
-		if (tab === 'likeList') {
-			return userLikeData.map((item) => {
-				return { ...item, isLikeByUser: true };
-			});
-		}
-	}, [isDataLoaded, isUserLikeDataLoaded, tweetsData, userLikeData]);
+	console.log('matchData', matchData);
 
 	return (
 		<div className={style.tweetList}>
-			{isDataLoaded && isUserLikeDataLoaded ? (
+			{isTweetListMainDataLoaded ? (
 				matchData.map(
 					({
 						id,
@@ -149,23 +105,19 @@ export const TweetList = ({ tab }) => {
 						LikedCounts,
 						RepliesCounts,
 						User,
-						Tweet,
+						// Tweet,
 						isLikeByUser,
 					}) => {
-						const hour = convertDateToHours(createdAt);
+						const createHour = convertDateToHours(createdAt);
 						return (
 							<TweetItem
 								key={id}
 								id={id}
-								description={
-									tab === 'tweetList' || tab === undefined ? description : Tweet.description
-								}
-								avatar={tab === 'tweetList' || tab === undefined ? User.avatar : Tweet.User.avatar}
-								name={tab === 'tweetList' || tab === undefined ? User.name : Tweet.User.name}
-								account={
-									tab === 'tweetList' || tab === undefined ? User.account : Tweet.User.account
-								}
-								createdAt={hour}
+								description={description}
+								avatar={User.avatar}
+								name={User.name}
+								account={User.account}
+								createdAt={createHour}
 								replyCounts={RepliesCounts}
 								likeCounts={LikedCounts}
 								isLikeByUser={isLikeByUser}
